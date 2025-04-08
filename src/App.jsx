36d7e706fb06f749c23d38473cce7baedfc3680f
Victor Revenco <victor.revenco@isa.utm.md>
2025-04-08 11:18:19 +0300

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='border border-blue-500'>
+      <h1>Tailwind CSS is working!</h1>
+    </div>
+  );
+}
+
+export default App;
