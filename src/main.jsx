3ec529e6aebdf5c80984fa0fad32596be893a392
Kipitokisk <victor.revenoc.00@gmail.com>
2025-05-05 21:32:24 +0300

    Change to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
