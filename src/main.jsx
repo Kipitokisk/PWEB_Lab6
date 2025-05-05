3d7e6cb71e3a6f3123ca71a65ed5495e13e03c87
Kipitokisk <victor.revenoc.00@gmail.com>
2025-05-05 21:42:32 +0300

    Redeploy on vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
 );
